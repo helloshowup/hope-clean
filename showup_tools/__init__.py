1183a181ad8d5a22c993c227ef77b92b653288c4
helloshowup <132871062+helloshowup@users.noreply.github.com>
2025-07-11 14:33:52 +0200

    Add refinement stage with prompts and tests

--- showup_tools/__init__.py
+++ showup_tools/__init__.py
@@ -1,2 +1,8 @@
 # Initialize the simplified_app package
 from .planning_stage import run_planning_stage
+from .refinement_stage import run_refinement_stage
+
+__all__ = [
+    'run_planning_stage',
+    'run_refinement_stage',
+]
